5c06a072d1235046de4aca8f0e83fa6094eebd38
Ahmed Wagih <ahmedwagih087@gmail.com>
2024-05-07 01:12:57 +0300

    icon

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,10 @@ import "./globals.css";
 export const metadata = {
   title: "Ahmed Wagih",
   description: "Welcome to my portfolio",
+  icons: {
+    icon: "/favicon.ico",
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({ children }) {
