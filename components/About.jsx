9182e68549b6e96039270a7292e04913dfd4091a
AhmeWagih <ahmedwagih087@gmail.com>
2024-10-28 17:58:14 +0300

    about

--- components/About.jsx
+++ components/About.jsx
@@ -31,24 +31,22 @@ const About = () => {
             variants={slideInFromLeft(0.8)}
             className="py-2 text-gray-600 leading-loose"
           >
-            I specialize in building mobile responsive front-end UI applications
-            that connect with API’s and other backend technologies. I’m
-            passionate about learning new technologies and understand there is
-            more than one way to accomplish a task. Though I am most proficient
-            in building front-end applications using HTML, CSS, Javascript, and
-            React, I am a quick learner and can pick up new tech stacks as
-            needed. I believe that being a great developer is not using one
-            specific language, but choosing the best tool for the job.
+            I specialize in building mobile-responsive front-end UI applications that integrate seamlessly with APIs and various backend technologies. 
+            My passion for learning new technologies drives me to explore multiple solutions to a problem. 
+            Although I am most proficient in creating front-end applications using HTML, CSS, JavaScript, React, and Next.js .
+            I pride myself on being a quick learner who can adapt to new tech stacks as required. 
+            I believe that being an effective developer is about selecting the right tools for each 
+            project rather than sticking to a single programming language.
           </motion.div>
-          <motion.div
+          {/* <motion.div
             variants={slideInFromLeft(1)}
             className="py-2 text-gray-600 leading-loose"
           >
             I started web developement in 2022 managing multiple e-commerce
             websites on CMS platforms such as WordPress, BigCommerce, and
             Shopify. I have experience working directly with clients and taking
-            mock wireframes all the way to deployed applications. 
-          </motion.div>
+            mock wireframes all the way to deployed applications.
+          </motion.div> */}
           <Link href="/projects">
             <motion.div
               variants={slideInFromLeft(1.2)}
